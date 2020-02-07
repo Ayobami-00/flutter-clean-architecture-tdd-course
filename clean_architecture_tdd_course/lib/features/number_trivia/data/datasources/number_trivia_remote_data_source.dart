64186e81e002ac4e83cdfcf88394102a83325c94
Festus <festusowumi@gmail.com>
2020-02-07 08:20:58 +0100

    Local Data Sources Implementation

--- clean_architecture_tdd_course/lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ clean_architecture_tdd_course/lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,6 +1,12 @@
-import 'package:clean_architecture_tdd_course/features/number_trivia/data/models/number_trivia_model.dart';
+import 'dart:convert';
 
-abstract class NumberTriviaRemoteDataSource{
+import 'package:clean_architecture_tdd_course/core/error/exceptions.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:meta/meta.dart';
+
+abstract class NumberTriviaRemoteDataSource {
   /// Calls the http://numbersapi.com/{number} endpoint.
   ///
   /// Throws a [ServerException] for all error codes.
@@ -10,4 +16,29 @@ abstract class NumberTriviaRemoteDataSource{
   ///
   /// Throws a [ServerException] for all error codes.
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({@required this.sharedPreferences});
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = sharedPreferences.getString('CACHED_NUMBER_TRIVIA');
+    if (jsonString != null) {
+      return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    return sharedPreferences.setString(
+      CACHED_NUMBER_TRIVIA,
+      json.encode(triviaToCache.toJson()),
+    );
+  }
 }
